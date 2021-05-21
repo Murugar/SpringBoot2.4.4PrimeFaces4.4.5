d5d54abebb724294916962f804d4d50aa81ffa6b
Muruga <davanon2014@gmail.com>
2021-05-21 16:06:37 +0530

    First

---
+++ src/main/java/com/iqmsoft/primefaces/TestPrimeFaces.java
@@ -0,0 +1,8 @@
+package com.iqmsoft.primefaces;
+
+import javax.inject.Named;
+
+@Named
+public class TestPrimeFaces {
+    private String message = "Greeting from IQMSOFT Join Faces Prime Faces";
+}
